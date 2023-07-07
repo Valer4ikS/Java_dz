2234537946e36d9180b3a81cf79c3706dc5740a8
Valer4ikS <maraclee@yandex.ru>
2023-07-07 07:57:37 +0300

    f15

--- seminar6/task1.java
+++ seminar6/task1.java
@@ -1,74 +1,85 @@
 package seminar6;
 
-// Разработать программу, имитирующую поведение коллекции HashSet. 
-// В программе содать метод add добавляющий элемент, 
-// метод toString возвращающий строку с элементами множества и метод позволяющий читать элементы по индексу.
-// *Реализовать все методы из семинара.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.Iterator;
+import java.util.Map;
 
-import java.util.*;
-
+/*
+Разработать программу, имитирующую поведение коллекции HashSet. 
+В программе содать метод add добавляющий элемент, метод toString 
+возвращающий строку с элементами множества и метод позволяющий 
+читать элементы по индексу.
+*Реализовать все методы из семинара.
+Формат данных Integer.
+ */
 public class task1 {
+  public static void main(String[] args) {
+    SetImitation setImitation = new SetImitation();
+    System.out.println(setImitation.add(9));
+    System.out.println(setImitation.add(9));
+    System.out.println(setImitation.add(4));
+    System.out.println(setImitation.add(3));
+    System.out.println(setImitation.add(1));
+    System.out.println(setImitation.delete(9));
+    System.out.println(setImitation.delete(9));
+    System.out.println(setImitation.size());
+    System.out.println(setImitation.isEmpty());
+    System.out.println(setImitation.contains(9));
+    System.out.println(setImitation.contains(4));
 
-    public static void main(String[] Args) {
-        SetImitation setImitation = new SetImitation();
-        System.out.println(setImitation.add(9));
-        System.out.println(setImitation.add(9));
-        System.out.println(setImitation.add(2));
-        System.out.println(setImitation.size());
-        System.out.println(setImitation.delete(9));
-        System.out.println(setImitation.delete(9));
-        System.out.println(setImitation.size());
-        System.out.println(setImitation.isEmpty());
-        System.out.println(setImitation.contains(9));
-        System.out.println(setImitation.contains(2));
-        for (int i = 0; i < 10; i++) {
-            setImitation.add(new Random().nextInt(1000));
-        }
-
-        System.out.println(setImitation.toString());
-        System.out.println(setImitation.getElementByIndex(3));
+    for (int i = 0; i < 10; i++) {
+      setImitation.add(new Random().nextInt(1000));
     }
+    Iterator<Integer> iterator = setImitation.iterator();
+    while (iterator.hasNext()) {
+      int i = iterator.next();
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    System.out.println(setImitation.getElementByIndex(3));
+    System.out.println(setImitation.toStr());
+  }
 }
 
-class SetImitation<E> {
-    private HashMap<E, Object> map = new HashMap<>();
-    private static final Object OBJECT = new Object();
+class SetImitation<E> { // var
+  private HashMap<E, Object> map = new HashMap<>();
 
-    public boolean add(E num) {
-        return map.put(num, OBJECT) == null;
-    }
+  private static final Object OBJECT = new Object();
 
-    public boolean delete(E num) {
-        return map.remove(num, OBJECT);
-    }
+  public boolean add(E num) {
+    return map.put(num, OBJECT) == null;
+  }
 
-    public int size() {
-        return map.size();
-    }
+  public boolean delete(E num) {
+    return map.remove(num, OBJECT);
+  }
 
-    public boolean isEmpty() {
-        return map.isEmpty();
-    }
+  public int size() {
+    return map.size();
+  }
 
-    public boolean contains(Object num) {
-        return map.containsKey(num);
-    }
+  public boolean isEmpty() {
+    return map.isEmpty();
+  }
 
-    public Iterator<E> iterator() {
-        return map.keySet().iterator();
-    }
+  public boolean contains(Object num) {
+    return map.containsKey(num);
+  }
 
-    public E getElementByIndex(int index) {
-        E[] mapArray = (E[]) map.keySet().toArray();
-        return mapArray[index];
-    }
+  public Iterator<E> iterator() {
+    return map.keySet().iterator();
+  }
 
-    public String toString() {
-        String result = "";
-        Iterator<E> iterator = map.keySet().iterator();
-        while (iterator.hasNext()) {
-            result += iterator.next().toString() + " ";
-        }
-        return result.strip();
-    }
+  public E getElementByIndex(int index) {
+    E[] mapArray = (E[]) map.keySet().toArray();
+    return mapArray[index];
+  }
+
+  public String toStr() {
+    return String.valueOf(map.keySet());
+  }
 }
